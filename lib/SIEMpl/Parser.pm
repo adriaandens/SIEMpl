98321b73a924999d3fc436e65e8f0ce289846a28
Adriaan <adriaan.dens@gmail.com>
2023-08-04 22:16:49 +0200

    Implement a year guess for logs without "year" in their timestamp

--- lib/SIEMpl/Parser.pm
+++ lib/SIEMpl/Parser.pm
@@ -29,11 +29,9 @@ class SIEMpl::Parser {
 
 	# Default timestamp looks like: Aug  2 16:01:02
 	# Returns a hashref with "epoch" and "log" as keys.
-	method base_parsing($line) {
+	# Implementation comment: DateTime gives back weird time because we don't have a Year in our string, so it thinks we are in year 1. So we'll assume it's a log about this year (this == run time) and if it turns out that the epoch generated is bigger than "now", we decrease the year by one. So if we are January 13 and we are reading Dec 25, we first try for our year Dec 25, figure out this is in the future, and then generate an epoch for Dec 25 last year. This does mean we can not have more than 1 year of logs before giving out wrong dates to events.
+	method base_parsing($line, $year = localtime->strftime('%Y') ) {
 		my ($date, $log) = $line =~ m/^(\w+\s+\d+\s+\d+:\d+:\d+)\s+(.*)$/;
-		# DateTime gives back weird time because we don't have a Year in our string, so it thinks we are in year 1....
-		# TODO: If we take the current year but we are January 1st and we are parsing something from yesterday, then the year is wrong! Might be best to assume (even if we are in July "right now") that everything that gets parsed as an epoch bigger than "now" is wrong. Logs are about the passed, not the future.
-		my $year = localtime->strftime("%Y");
 		# %b or %B or %h: The month name according to the given locale, in abbreviated form or the full name.
 		# %d or %e: The day of month (01-31). This will parse single digit numbers as well.
 		my $format = "%Y %b %d %T";
@@ -44,6 +42,11 @@ class SIEMpl::Parser {
 			on_error => 'croak'
 		);
 		my $dt = $strp->parse_datetime($year . ' ' . $date);
+		my $epoch = strftime("%s", $dt);
+		my $now = localtime->strftime("%s");
+		if($epoch > $now) { # The event is in the FUTURE
+			return $self->base_parsing($line, $year - 1); # Try a year less
+		}
 
 		my %event = ("epoch" => strftime("%s", $dt), "log" => $log);
 		return \%event; 
